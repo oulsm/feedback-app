2b79d4e88e7c7769e3e0c5627376a21c9f8eae4f
oulsm <s1252701@ouhk.edu.hk>
2022-08-21 17:01:18 +0800

    17_00_21_08_22_update
    new version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import React from "react";
 function App() {
     return(
-        <div className="container">
-             <h1> My App</h1>
-        <p>Hello</p>
-        </div>
+        React.createElement('div',{className: 'container'},
+        React.createElement('h1',{ }, 'Myapp'))
        
     ) 
 }
