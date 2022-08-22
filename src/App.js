9391c9bf98370927543b74c92fb6e285c1fb0e99
oulsm <s1252701@ouhk.edu.hk>
2022-08-22 15:52:32 +0800

    15_25_22_8_22_update
    Dynamic Values & LIsts in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+
 function App() {
+    const title = 'Blog Post'
+    const body = 'This is my blog'
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
     return(
-        React.createElement('div',{className: 'container'},
-        React.createElement('h1',{ }, 'Myapp'))
+        <div className="container">
+             <h1> {title}</h1>
+        <p>{body}</p>
+
+        <div className="comments">
+        <h3> Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) =>(
+                <li key ={index}>{comment.text}</li>
+            ))}
+        </ul>
+        </div>
+        </div>
        
     ) 
 }
