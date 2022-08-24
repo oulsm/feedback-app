fd4f758268e15a482c54f68c93d9fd6b2bb9cfaf
oulsm <s1252701@ouhk.edu.hk>
2022-08-24 22:05:01 +0800

    22_04_24_08_22_update
    .Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ function App() {
         {id: 2, text: 'Comment two'},
         {id: 3, text: 'Comment three'},
     ]
+
+    const loading = false
+    const showComments = true
+    const commentBlock = (<div className="comments">
+    <h3> Comments ({comments.length})</h3>
+    <ul>
+        {comments.map((comment, index) =>(
+            <li key ={index}>{comment.text}</li>
+        ))}
+    </ul>
+    </div>)
+    if(loading) return <h1>Loading....</h1>
+    
     return(
         <div className="container">
              <h1> {title}</h1>
         <p>{body}</p>
-
-        <div className="comments">
-        <h3> Comments ({comments.length})</h3>
-        <ul>
-            {comments.map((comment, index) =>(
-                <li key ={index}>{comment.text}</li>
-            ))}
-        </ul>
-        </div>
+        {showComments && commentBlock }
+        
         </div>
        
     ) 
