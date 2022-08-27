c168a0341008f58d1fa32bbf953a0ee8b2b6c86b
oulsm <s1252701@ouhk.edu.hk>
2022-08-27 12:58:41 +0800

    12_58_27_8_22_update
    First components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
+import Header from "./components/Header";
 
 function App() {
     const title = 'Blog Post'
-    const body = 'This is my blog'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = false
-    const showComments = true
-    const commentBlock = (<div className="comments">
-    <h3> Comments ({comments.length})</h3>
-    <ul>
-        {comments.map((comment, index) =>(
-            <li key ={index}>{comment.text}</li>
-        ))}
-    </ul>
-    </div>)
-    if(loading) return <h1>Loading....</h1>
     
+
     return(
+        <>
+        <Header />
         <div className="container">
-             <h1> {title}</h1>
-        <p>{body}</p>
-        {showComments && commentBlock }
-        
+          <h1>My App</h1>
         </div>
+    </>
        
     ) 
 }
