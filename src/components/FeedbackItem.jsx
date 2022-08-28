1e3158d5de5243780eb1f90eb2327f23314ce14a
oulsm <s1252701@ouhk.edu.hk>
2022-08-28 19:36:03 +0800

    19_35_28_8_88_update
    Events & Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,16 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from "prop-types"
 import Card from "./shared/Card"
-function FeedbackItem({item}) {
-   // const [rating, setRating] = useState(7)
-    //const [text, setText] = useState('This is an example of a feedback item')
-  
-    
+function FeedbackItem({item ,handleDelete}) {
+    const handleClick = () =>  {
+
+    }
     return (
     <Card >
         <div className="num-display">{item.rating}</div>
+        <button className="close">
+            <FaTimes onClick={() => handleDelete(item.id)} color='purple'  />
+        </button>
         <div className="text-display">{item.text}</div>
        
     </Card>
