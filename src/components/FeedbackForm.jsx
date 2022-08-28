48c69c321773ae6ce98c266c926a954625ac01e9
oulsm <s1252701@ouhk.edu.hk>
2022-08-29 02:03:28 +0800

    02_03_29_8_88_update
    Add Feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from "./shared/Card"
 import RatingSelect from "./RatingSelect"
 import {useState} from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisable] = useState(true)
@@ -22,9 +22,20 @@ function FeedbackForm() {
         }
         setText(e.target.value)
     }
+    const handlsSubmit = (e) =>{
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                text,
+                rating, 
+            }
+            handleAdd(newFeedback)
+            setText('')
+        }
+    }
   return (
     <Card>
-      <form>
+      <form onSubmit={handlsSubmit}>
         <h2> How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
