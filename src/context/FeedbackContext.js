da273caa40392ff786b019d0483da769727213c2
oulsm <s1252701@ouhk.edu.hk>
2022-08-29 17:18:53 +0800

    17_18_29_8_22_update
     Edit Feedback Event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -21,20 +21,34 @@ export const FeedbackProvider = ({children}) =>  {
         }
         
     ])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false,
+    })
+    // Add feedback
     const addFeedback = (newFeedback) =>{
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
+    //Delete feedback
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')){
             setFeedback(feedback.filter((item) => item.id !== id))
 
         }
     }
+    // Set item to updated
+    const editFeedback = (item) =>{
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
     return <FeedbackContext.Provider value={{
     feedback,
     deleteFeedback,
     addFeedback,
+    editFeedback,
     }}>
         {children}
 
