546f30854d684ee81f1cb1071acea4135afc3885
oulsm <s1252701@ouhk.edu.hk>
2022-08-29 19:19:10 +0800

    29_18_29_8_22_update
    Fetch Data From JSON-Server Backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,36 +1,24 @@
-import {createContext,  useState} from 'react'
+import {createContext,  useState, useEffect} from 'react'
 import { v4 as uuidv4} from 'uuid'
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) =>  {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item 1',
-            rating: 10, 
-        },
-        {
-            id: 2,
-            text: 'This item 2',
-            rating: 9, 
-        },
-        {
-            id: 3,
-            text: 'This item 3',
-            rating: 8, 
-        }
-        ,
-        {
-            id: 4,
-            text: 'This item 4',
-            rating: 2, 
-        }
-        
-    ])
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     })
+    useEffect(() => {
+        fetchFeedback()
+    },[])
+    //Fetch feed back
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+    }
+
     // Add feedback
     const addFeedback = (newFeedback) =>{
         newFeedback.id = uuidv4()
